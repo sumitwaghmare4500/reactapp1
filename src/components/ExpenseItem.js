ae0b0f9fec0bb48288d5c0dbb7588700a390d7f4
sumitwaghmare4500 <sumitwaghmare8083@gmail.com>
2023-08-02 12:11:49 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return(<div>
+        <div><h2>car insurance</h2></div>
+        <div>Food Rs 10</div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+    </div>
+   )
+      
+}
+export default ExpenseItem;
